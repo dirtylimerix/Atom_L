4229c93015b640ca7018262ed133078c2213df27
James Brock <jamesbrock@livefreeordienh.com>
2020-11-08 12:21:36 -0500

    Updating build scripts to fix typo in release file copying and changing BoardConfig.mk to exclude SELinux to test recovery build for other users.

--- newbuild.sh
+++ newbuild.sh
@@ -54,8 +54,8 @@ DATEVAL=`ls lineage-17.1-*-UNOFFICIAL-Atom_L.zip | awk -F '[-.]' '{print $4}'`
 cd $TOP_DIR
 
 mkdir $TOP_DIR/releases/$DATEVAL
-cp $LINEAGE_DIR/out/target/product/Atom_L/lineage-17.1-$DATEVAL-UNOFFICIAL-Atom_L.zip $TOP_DIR/$DATEVAL/releases
-cp $LINEAGE_DIR/out/target/product/Atom_L/lineage-17.1-$DATEVAL-UNOFFICIAL-Atom_L.zip.md5sum $TOP_DIR/$DATEVAL/releases
+cp $LINEAGE_DIR/out/target/product/Atom_L/lineage-17.1-$DATEVAL-UNOFFICIAL-Atom_L.zip $TOP_DIR/releases/$DATEVAL
+cp $LINEAGE_DIR/out/target/product/Atom_L/lineage-17.1-$DATEVAL-UNOFFICIAL-Atom_L.zip.md5sum $TOP_DIR/releases/$DATEVAL
 cp $LINEAGE_DIR/out/target/product/Atom_L/recovery.img $TOP_DIR/releases/$DATEVAL/lineage-17.1-$DATEVAL-recovery.img
 
 
